2c1c504b84ff91b40377e4115730fd4a1378b123
realkevinbrian <realkevinbrian@gmail.com>
2023-04-21 09:20:03 +0200

    stack route

--- routes/stack.route.js
+++ routes/stack.route.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import CatalogScreen from "../screens/CatalogScreen";
+
+//init stack
+const Stack = createNativeStackNavigator();
 
 function StackRoute() {
-  return
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Category" component={CatalogScreen} />
+    </Stack.Navigator>
+  );
 }
 export default StackRoute;
